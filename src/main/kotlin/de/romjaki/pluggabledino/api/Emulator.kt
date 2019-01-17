0dbfc25385db3df3cfb0d7500e285795f50f4387
Tyrannova <daniel.sluga@grb-online.net>
2019-01-17 23:20:41 +0100

    birds arent real

--- src/main/kotlin/de/romjaki/pluggabledino/api/Emulator.kt
+++ src/main/kotlin/de/romjaki/pluggabledino/api/Emulator.kt
@@ -3,10 +3,13 @@ package de.romjaki.pluggabledino.api
 import de.romjaki.pluggabledino.game.GameWorld
 
 object Emulator {
-    fun emulate(delta: Float, shouldJump: (GameWorld) -> Boolean): Float {
+    fun emulate(delta: Float, shouldJump: (GameWorld) -> Boolean, deleteBirds: Boolean): Float {
         val world = GameWorld()
         var score = 0f
         while (!world.hurt) {
+            if (deleteBirds) {
+                world.birdd.clear()
+            }
             world.update(delta / 1000f, shouldJump(world))
             score += delta
         }
